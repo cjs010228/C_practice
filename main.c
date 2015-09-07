cdeb966fd3c81f86c2f5f63fa3529c829b372b74
Brandon <brandon@brandon-Aspire-V3-772G.(none)>
2015-09-07 17:57:06 +0800

    atoi convert string to int

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include "string.h"
-#define my_name "Brandon"
+#include <stdbool.h>//It is essential to use the bool variable.
 
 int main()
 {
-	char s1[] = my_name;
-	char s2[20];
-    strcpy(s2,s1);//Both of the two variable need to occupy the memory, and could not be a pointer.
-    printf("%s\n",s2);
+    char num_string[]="76";
+	bool is_int = true;
+	int convert_int;
+	char convert_char;
+
+    /*Int and char could both catch the integer, but the char has small range, 0~255 */
+	convert_int = atoi(&num_string);//the argument needs to be pointer 
+	convert_char = atoi(&num_string);
+	printf("int save %d, sizeof = %ld\n", convert_int, sizeof(convert_int));
+    printf("char save %d, sizeof = %ld\n", convert_char, sizeof(convert_char));
+
+    /*Bool could also save int, but it only save 0 or 1*/
+	printf("True is the integer: %d, sizeof = %ld\n",is_int,sizeof(is_int));
+
     return 0;
 }
 
