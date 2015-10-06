afa42e33bdef33122d6ae330caa1cf6ebb85d49d
Brandon <brandon@brandon-Aspire-V3-772G.(none)>
2015-10-06 11:38:03 +0800

    selection sort

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
-#include "name_print.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define NUM 10
+#define swap(a,b) { if(a!=b) {a^=b, b^=a, a^=b;} }
 
 int main()
 {
-    name_print(my_name);
+    int *array=malloc(NUM);
+    int i;
+    srand((unsigned)time(NULL));
+    for(i=0;i<NUM;i++)    
+    {
+        *(array+i)=rand()%100;
+    }
+    
+    for(i=0;i<NUM;i++)   
+        printf("array[%d] = %d\n",i,*(array+i));
+    
+    printf("\n");
+    
+    for(i=0;i<NUM;i++)
+    {
+        int j;
+        int smallest_index=i;       
+        for(j=i;j<NUM;j++)
+        {
+            if(*(array+smallest_index)>*(array+j))
+            {
+                smallest_index = j;
+            }
+        }
+        swap(*(array+i),*(array+smallest_index));
+    }
+    
+    for(i=0;i<NUM;i++)   
+        printf("array[%d] = %d\n",i,*(array+i));
+
     return 0;
 }
 
-/*void name_print(char* name)
-{
-    printf("%s\n",name);
-}*/
