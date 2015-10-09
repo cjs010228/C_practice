bf6f3bef3398f6dc89d863158ed0b933fd798908
cjs010228 <Brandon>
2015-10-09 16:51:58 +0800

    Find smallest_character

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
-#include "name_print.h"
+#include <stdio.h>
+#include <string.h>
+char smallest_character(char*);
 
 int main()
 {
-    name_print(my_name);
+    char smallest_chr;
+    char test_string[]="HeythisisBrandon";
+    printf("size is %lu\n",strlen(test_string));
+    smallest_chr=smallest_character(test_string);
+    printf("smallest is '%c' %d\n",smallest_chr,smallest_chr);
     return 0;
 }
 
-/*void name_print(char* name)
+char smallest_character(char* str)
 {
-    printf("%s\n",name);
-}*/
+    int i=0, smallest_index=0;
+    printf("size is %lu\n",strlen(str));
+    while(1)
+    {
+        if(str[smallest_index] >= str[i])
+            smallest_index = i;
+        i++;
+        if(str[i]=='\0')
+            break;        
+        
+        
+    }
+    return str[smallest_index];
+}
+
