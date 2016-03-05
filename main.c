c8a72356e111a7360f2212607030bc314ffc2574
cjs010228 <Brandon>
2016-03-05 11:23:00 +0800

    Parse GNGGA message

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#define PARSESTRING "$GNGGA"
 
 int main(void)
 {
-    printf("Hello world!\n");
+    FILE *article;
+    article = fopen("article.txt","r");
+    char *s;
+    char buffer[4096]; 
+    fread(buffer,4096,1,article);
+
+    s = strtok(buffer,"\n");
+    while(s!=NULL)
+    {
+        if(!strncmp(s,PARSESTRING,strlen(PARSESTRING)))
+            printf("%s\n",s);
+        s =strtok(NULL,"\n"); 
+    }
+    
+
     return 0;
+
 }
