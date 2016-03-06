13a24e4dfc790400ed184082761a4caf7a205c65
cjs010228 <Brandon>
2016-03-06 21:11:03 +0800

    Generate the student list with score

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#define STUDENT_NUM 45
+
+typedef struct student{
+    int id;
+    int score;
+}SInfo;
+
+void Sort1(SInfo*);
+
+int main()
 {
-    printf("Hello world!\n");
+    srand(0);
+
+    int id = 0;
+    SInfo student[STUDENT_NUM];
+    for(id = 1; id <=STUDENT_NUM; id++)
+    {
+        student[id-1].id=id;
+        student[id-1].score=rand()%100;
+    }
+
+    Sort1(&student);
+
     return 0;
+}
+
+void Sort1(SInfo* student)
+{
+    int i;
+    //for(i = 0; i<sizeof(*student)/sizeof(SInfo); i++)
+    for(i = 0; i<STUDENT_NUM; i++)
+    {
+        printf("%d %d\n",student[i].id,student[i].score);
+    }
 }
