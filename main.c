4cc147abed8cdd50c4458c09ac03aaef46d19251
Brandon <830257@IoT-830257.ms.cloudnsbg.efoxconn.com>
2016-03-15 19:17:11 +0800

    Bubble sort complete

--- main.c
+++ main.c
@@ -6,36 +6,48 @@
 
 int main()
 {
-    int *array=malloc(NUM);
-    int i;
-    srand((unsigned)time(NULL));
-    for(i=0;i<NUM;i++)    
-    {
-        *(array+i)=rand()%100;
-    }
-    
-    for(i=0;i<NUM;i++)   
-        printf("array[%d] = %d\n",i,*(array+i));
-    
-    printf("\n");
-    
-    for(i=0;i<NUM;i++)
-    {
-        int j;
-        int smallest_index=i;       
-        for(j=i;j<NUM;j++)
-        {
-            if(*(array+smallest_index)>*(array+j))
-            {
-                smallest_index = j;
-            }
-        }
-        swap(*(array+i),*(array+smallest_index));
-    }
-    
-    for(i=0;i<NUM;i++)   
-        printf("array[%d] = %d\n",i,*(array+i));
+	int *array=malloc(NUM);
+	int i, j, float_idx,k,counter=0;
 
-    return 0;
+	srand((unsigned)time(NULL));
+	for(i=0;i<NUM;i++)    
+	{
+		*(array+i)=rand()%100;
+	}
+	
+	while(1)
+	{
+		/*Find bubble ont the bottom*/
+		for(float_idx=0;float_idx<(NUM-1);float_idx++)
+		{
+			if(*(array+(float_idx+1)) >= *(array+float_idx))
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		/*No bubble*/
+		if(float_idx == NUM-1)
+			break;
+	
+        /*Float to the top*/
+		for(j=float_idx;j<(NUM-1);j++)
+		{
+			if(*(array+(j+1)) >= *(array+j))
+				break;
+			else
+				swap(*(array+j),*(array+(j+1)));  
+		}  
+
+		for(k=NUM-1;k>=0;k--) {printf("%d ",*(array+k));}
+		printf("\n");   
+ 
+	}
+
+	return 0;
 }
 
