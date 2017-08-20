7b0b51b4d111d80c3627ec7a7ea88fc50ec089bc
cjs010228 <Brandon>
2017-08-20 22:36:35 +0800

    Change specfic bit to be 0 or 1

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-    printf("Hello world!\n");
+    /* Bit mask: Change specific bit to be 1 */
+    unsigned char output = 0, input = 0;
+    output = input | 0x80;
+    printf("%2x\n", output);
+
+    /* Bit mask: Change specific bit to be 0 */
+    output = input & 0x7F;
+    printf("%2x\n", output);
+
     return 0;
 }
